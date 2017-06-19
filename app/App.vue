6369b000ba642029c694fc89dcecddddaebcf440
ChalkPE <chalk@chalk.pe>
2017-06-19 11:00:29 +0900

    Add Lato webfont

--- app/App.vue
+++ app/App.vue
@@ -11,12 +11,10 @@
 </template>
 
 <script>
-  import './app.sass'
   import 'noto-sans-kr'
-  import 'font-awesome/css/font-awesome.css'
+  import 'lato-webfont'
 
   import ResumeNav from './components/Navigation.vue'
-
   import ResumeLikes from './components/grid/Likes.vue'
   import ResumeContact from './components/grid/Contact.vue'
 
@@ -26,4 +24,24 @@
 </script>
 
 <style lang="scss">
+  $primary: #1abd9d;
+  $family-monospace: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+  $family-sans-serif: Lato, Roboto, Oxygen, Ubuntu, "Fira Sans", "Droid Sans", "Helvetica Neue", Helvetica, Arial, "Noto Sans Korean", sans-serif;
+
+  @import "~bulma";
+  @import '~font-awesome/css/font-awesome.css';
+
+  *:focus {
+    outline: none;
+  }
+
+  #app {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  section {
+    flex: 1 0 auto;
+  }
 </style>
