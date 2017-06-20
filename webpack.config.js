d900bbf7a95fb66971446000f876412aeed2a1c3
ChalkPE <chalk@chalk.pe>
2017-06-20 16:44:29 +0900

    Production build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,46 @@
 const path = require('path')
+const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const plugins = [
+  new ExtractTextPlugin('bundle.css')
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: '"production"' }
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: { warnings: false }
+    }),
+
+    new webpack.LoaderOptionsPlugin({ minimize: true }))
+}
+
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options: { sourceMap: 'inline' }
+}
 
 const loaders = {
   html: 'pug-loader',
 
   css: ExtractTextPlugin.extract({
     fallback: 'vue-style-loader',
-    use: ['css-loader', 'postcss-loader']
+    use: ['css-loader', postcssLoader]
   }),
 
   scss: ExtractTextPlugin.extract({
     fallback: 'vue-style-loader',
-    use: ['css-loader', 'sass-loader', 'postcss-loader']
+    use: ['css-loader', 'sass-loader', postcssLoader]
   }),
 
   sass: ExtractTextPlugin.extract({
     fallback: 'vue-style-loader',
-    use: ['css-loader', 'sass-loader?indentedSyntax', 'postcss-loader']
+    use: ['css-loader', 'sass-loader?indentedSyntax', postcssLoader]
   })
 }
 
@@ -43,7 +67,7 @@ const rules = [
     test: /\.css$/,
     use: ExtractTextPlugin.extract({
       fallback: 'style-loader',
-      use: ['css-loader', 'postcss-loader']
+      use: ['css-loader', postcssLoader]
     })
   },
 
@@ -51,7 +75,7 @@ const rules = [
     test: /\.s[ac]ss$/,
     use: ExtractTextPlugin.extract({
       fallback: 'style-loader',
-      use: ['css-loader', 'sass-loader', 'postcss-loader']
+      use: ['css-loader', 'sass-loader', postcssLoader]
     })
   }
 ]
@@ -65,10 +89,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
 
-  plugins: [
-    new ExtractTextPlugin('bundle.css')
-  ],
-
+  plugins,
   module: { rules },
 
   resolve: {
@@ -79,5 +100,7 @@ module.exports = {
     hot: true,
     host: '0.0.0.0',
     historyApiFallback: true
-  }
+  },
+
+  devtool: '#source-map'
 }
