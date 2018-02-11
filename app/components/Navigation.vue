3a31c7138e9d643e225e0a2f72d0c770a7fe5ec0
ChalkPE <chalk@chalk.pe>
2018-02-11 16:52:02 +0000

    use https

--- app/components/Navigation.vue
+++ app/components/Navigation.vue
@@ -6,7 +6,7 @@
 
     .modal(:class='open && `is-active`')
       .modal-background(@click='open = false')
-      .modal-content: p.image: img#face(src='http://i.imgur.com/JaIkDmv.jpg')
+      .modal-content: p.image: img#face(src='https://i.imgur.com/JaIkDmv.jpg')
       button.modal-close(@click='open = false')
 </template>
 
