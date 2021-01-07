1efe698de16daa50282ca6343ab854cfc77d4cd9
Chalk <chalkpe@gmail.com>
2021-01-07 13:21:49 +0900

    feat: add graduation

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -12,6 +12,12 @@ export default [
   },
 
   {
+    text: '한국디지털미디어고등학교',
+    icon: 'graduation-cap',
+    href: 'https://dimigo.hs.kr'
+  },
+
+  {
     text: '2000년 6월 17일',
     icon: 'birthday-cake',
     href: '#'
